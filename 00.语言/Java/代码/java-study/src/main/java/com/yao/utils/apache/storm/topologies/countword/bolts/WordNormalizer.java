1c5ad8dc73bd7f0d63d02265e3e570d127ab51ea
yaoqingju <yaoqingju@91yong.com>
2015-10-08 18:30:38 +0800

    提交 storm 的不同来源和处理方法的实现

---
+++ 00.语言/Java/代码/java-study/src/main/java/com/yao/utils/apache/storm/topologies/countword/bolts/WordNormalizer.java
@@ -0,0 +1,46 @@
+package com.yao.utils.apache.storm.topologies.countword.bolts;
+
+import java.util.Map;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class WordNormalizer extends BaseRichBolt {
+
+	private OutputCollector collector;
+	int numCounterTasks=0;
+	public void cleanup() {}
+
+	/**
+     * 将传入的键值做好分割切换并重新组成键值对传入下一流程
+	 */
+    public void execute(Tuple input) {
+        String sentence = input.getString(0);
+        String[] words = sentence.split(" ");
+        for(String word : words){
+            word = word.trim();
+            if(!word.isEmpty()){
+                word = word.toLowerCase();
+                collector.emit(new Values(word));
+            }
+        }
+        // 通知 Storm 集群，当前这条信息已经处理到哪个环节 ->
+            // !!! 若没有进行 ack 或 fail 操作则最后会报 OutOfMemory 错误
+        collector.ack(input);
+    }
+
+	public void prepare(Map stormConf, TopologyContext context,
+			OutputCollector collector) {
+		this.collector = collector;
+		this.numCounterTasks = context.getComponentTasks("word-counter").size();
+	}
+
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("word"));
+	}
+}
---
+++ 00.语言/Java/代码/java-study/src/main/java/com/yao/utils/apache/storm/topologies/countword/spouts/WordReader.java
@@ -0,0 +1,68 @@
+package com.yao.utils.apache.storm.topologies.countword.spouts;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+
+public class WordReader extends BaseRichSpout {
+
+	private SpoutOutputCollector collector;
+	private FileReader fileReader;
+	private boolean completed = false;
+	private TopologyContext context;
+
+	public void ack(Object msgId) {
+		System.out.println("OK:"+msgId);
+	}
+	public void close() {}
+	public void fail(Object msgId) {
+		System.out.println("FAIL:"+msgId);
+	}
+
+	/**
+	 * 获取指定配置文件信息并转换为键值对输出到下一环节
+	 */
+	public void nextTuple() {
+		if(completed){
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				//Do nothing
+			}
+			return;
+		}
+		String str;
+		BufferedReader reader = new BufferedReader(fileReader);
+		try{
+			while((str = reader.readLine()) != null){
+				this.collector.emit(new Values(str),str);
+			}
+		}catch(Exception e){
+			throw new RuntimeException("Error reading tuple",e);
+		}finally{
+			completed = true;
+		}
+	}
+
+	public void open(Map conf, TopologyContext context,
+			SpoutOutputCollector collector) {
+		try {
+			this.context = context;
+			this.fileReader = new FileReader(conf.get("wordsFile").toString());
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException("Error reading file ["+conf.get("wordFile")+"]");
+		}
+		this.collector = collector;
+	}
+
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("line"));
+	}
+}
