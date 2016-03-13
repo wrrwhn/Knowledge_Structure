a075e416a1f1a70a369076a83be72f72558166a9
wrrwhn <yqjdcyy@gmail.com>
2016-03-14 01:35:37 +0800

    thread factory

---
+++ 00.语言/Java/代码/java-study/src/main/java/com/yao/study/thread/custom/threadFactory/Main.java
@@ -0,0 +1,20 @@
+package com.yao.study.thread.custom.threadFactory;
+
+/**
+ * Created by Yao on 2016/3/13.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        CustomFactory factory= new CustomFactory("Hello");
+
+        Thread thread = factory.newThread(new CustomTask());
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
