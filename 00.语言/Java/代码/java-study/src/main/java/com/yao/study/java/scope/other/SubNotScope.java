9e245d43e7ebafc94e93c12c27cd6dc7717f8931
wrrwhn <yqjdcyy@gmail.com>
2016-05-17 00:50:47 +0800

    scope test

---
+++ 00.语言/Java/代码/java-study/src/main/java/com/yao/study/java/scope/other/SubNotScope.java
@@ -0,0 +1,16 @@
+package com.yao.study.java.scope.other;
+
+import com.yao.study.java.scope.empty.NotScope;
+
+/**
+ * Creator: Yao
+ * Date:    2016/5/17
+ * For:
+ * Other:
+ */
+public class SubNotScope extends NotScope {
+
+    public SubNotScope() {
+//        System.out.println(this.val);
+    }
+}
